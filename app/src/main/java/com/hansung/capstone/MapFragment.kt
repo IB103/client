22da1d918b1005bf55b218c5ce3fa18961765e44
yootal <khyoo96@naver.com>
2023-02-07 13:37:53 +0900

    ㄹㄹ

--- app/src/main/java/com/hansung/capstone/MapFragment.kt
+++ app/src/main/java/com/hansung/capstone/MapFragment.kt
@@ -28,88 +28,9 @@ import retrofit2.Response
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        // 위치소스 권한 설정
-        locationSource =
-            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
-
-        // 초기 옵션대로 생성
-        val fm = childFragmentManager
-        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-            ?: MapFragment.newInstance().also {
-                fm.beginTransaction().add(R.id.map, it).commit()
-            }
-        mapFragment.getMapAsync(this)
-
-        val searchB = view?.findViewById<ImageButton>(R.id.locationSearchB)
-        searchB?.setOnClickListener {
-            // 빈칸일 때 입력 요청 토스트 추가 필요
-
-            // 전에 찍었던 마커 삭제
-            for (i in markers) {
-                i.map = null
-            }
-            markers.clear() // 리스트 비우기
-            val source = view?.findViewById<EditText>(R.id.locationSearch)
-//            if (source.text.toString() == "") {
-//                source.requestFocus()
-//                val manager: InputMethodManager =
-//                    getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
-//                manager.showSoftInput(source, InputMethodManager.SHOW_IMPLICIT)
-//                Toast.makeText(this@MapFragment, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
-//            } else {
-//                val manager = getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
-//                manager.hideSoftInputFromWindow(
-//                    currentFocus!!.windowToken,
-//                    InputMethodManager.HIDE_NOT_ALWAYS
-//                )
-            val api = NaverAPI.create()
-
-            api.getSearchLocation(source?.text.toString(), 10, 1)
-                .enqueue(object : Callback<ResultGetSearchLocation> {
-                    override fun onResponse(
-                        call: Call<ResultGetSearchLocation>,
-                        response: Response<ResultGetSearchLocation>
-                    ) {
-                        Log.d("결과", "성공 : ${response.body().toString()}")
-//                    // 값을 넣어야한다~
-                        val body = response.body()
-//
-//                    //Gson을 Kotlin에서 사용 가능한 object로 만든다.
-//                    val gson = GsonBuilder().create()
-//                    val searchResult = Gson().fromJson(body, ResultGetSearchLocation::class.java)
-                        val resultList = view?.findViewById<RecyclerView>(R.id.resultList)
-                        activity?.runOnUiThread {
-                            resultList?.adapter =
-                                body?.let { it -> RecyclerViewAdapter(it) }
-                        }
-                        //
-
-                        val wherex = body?.items?.get(0)?.mapx
-                        val wherey = body?.items?.get(0)?.mapy
-
-                        // 첫번째 검색 결과 좌표로 지도 이동
-                        var tm = Tm128(wherex!!.toDouble(), wherey!!.toDouble())
-                        val cameraUpdate = CameraUpdate.scrollTo(tm.toLatLng())
-                            .animate(CameraAnimation.Fly, 1000)
-                        naverMap.moveCamera(cameraUpdate)
-
-                        for (item in body.items) {
-                            tm = Tm128(item.mapx!!.toDouble(), item.mapy!!.toDouble())
-                            val marker = Marker()
-                            markers.add(marker)
-                            marker.position = tm.toLatLng()
-                            marker.map = naverMap
-                        }
-                    }
-
-                    override fun onFailure(call: Call<ResultGetSearchLocation>, t: Throwable) {
-                        Log.d("결과:", "실패 : $t")
-                    }
-                })
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//    }
 
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
@@ -139,85 +60,85 @@ class MapFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        // 위치소스 권한 설정
-//        locationSource =
-//            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        // 위치소스 권한 설정
+        locationSource =
+            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        // 초기 옵션대로 생성
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+
+        val searchB = view.findViewById<ImageButton>(R.id.locationSearchB)
+        searchB.setOnClickListener {
+            // 빈칸일 때 입력 요청 토스트 추가 필요
+
+            // 전에 찍었던 마커 삭제
+            for(i in markers){
+                i.map = null
+            }
+            markers.clear() // 리스트 비우기
+            val source = view.findViewById<EditText>(R.id.locationSearch)
+//            if (source.text.toString() == "") {
+//                source.requestFocus()
+//                val manager: InputMethodManager =
+//                    getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+//                manager.showSoftInput(source, InputMethodManager.SHOW_IMPLICIT)
+//                Toast.makeText(this@MapFragment, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
+//            } else {
+//                val manager = getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+//                manager.hideSoftInputFromWindow(
+//                    currentFocus!!.windowToken,
+//                    InputMethodManager.HIDE_NOT_ALWAYS
+//                )
+                val api = NaverAPI.create()
+
+                api.getSearchLocation(source.text.toString(), 10, 1)
+                    .enqueue(object : Callback<ResultGetSearchLocation> {
+                        override fun onResponse(
+                            call: Call<ResultGetSearchLocation>,
+                            response: Response<ResultGetSearchLocation>
+                        ) {
+                            Log.d("결과", "성공 : ${response.body().toString()}")
+//                    // 값을 넣어야한다~
+                            val body = response.body()
 //
-//        // 초기 옵션대로 생성
-//        val fm = childFragmentManager
-//        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
-//            ?: MapFragment.newInstance().also {
-//                fm.beginTransaction().add(R.id.map, it).commit()
-//            }
-//        mapFragment.getMapAsync(this)
-//
-//        val searchB = view.findViewById<ImageButton>(R.id.locationSearchB)
-//        searchB.setOnClickListener {
-//            // 빈칸일 때 입력 요청 토스트 추가 필요
-//
-//            // 전에 찍었던 마커 삭제
-//            for(i in markers){
-//                i.map = null
-//            }
-//            markers.clear() // 리스트 비우기
-//            val source = view.findViewById<EditText>(R.id.locationSearch)
-////            if (source.text.toString() == "") {
-////                source.requestFocus()
-////                val manager: InputMethodManager =
-////                    getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
-////                manager.showSoftInput(source, InputMethodManager.SHOW_IMPLICIT)
-////                Toast.makeText(this@MapFragment, "검색어를 입력해주세요", Toast.LENGTH_SHORT).show()
-////            } else {
-////                val manager = getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
-////                manager.hideSoftInputFromWindow(
-////                    currentFocus!!.windowToken,
-////                    InputMethodManager.HIDE_NOT_ALWAYS
-////                )
-//                val api = NaverAPI.create()
-//
-//                api.getSearchLocation(source.text.toString(), 10, 1)
-//                    .enqueue(object : Callback<ResultGetSearchLocation> {
-//                        override fun onResponse(
-//                            call: Call<ResultGetSearchLocation>,
-//                            response: Response<ResultGetSearchLocation>
-//                        ) {
-//                            Log.d("결과", "성공 : ${response.body().toString()}")
-////                    // 값을 넣어야한다~
-//                            val body = response.body()
-////
-////                    //Gson을 Kotlin에서 사용 가능한 object로 만든다.
-////                    val gson = GsonBuilder().create()
-////                    val searchResult = Gson().fromJson(body, ResultGetSearchLocation::class.java)
-//                            val resultList = view.findViewById<RecyclerView>(R.id.resultList)
-//                            activity?.runOnUiThread {
-//                                resultList.adapter =
-//                                    body?.let { it -> RecyclerViewAdapter(it) }
-//                            }
-//                            //
-//
-//                            val wherex = body?.items?.get(0)?.mapx
-//                            val wherey = body?.items?.get(0)?.mapy
-//
-//                            // 첫번째 검색 결과 좌표로 지도 이동
-//                            var tm = Tm128(wherex!!.toDouble(), wherey!!.toDouble())
-//                            val cameraUpdate = CameraUpdate.scrollTo(tm.toLatLng())
-//                                .animate(CameraAnimation.Fly, 1000)
-//                            naverMap.moveCamera(cameraUpdate)
-//
-//                            for (item in body.items) {
-//                                tm = Tm128(item.mapx!!.toDouble(), item.mapy!!.toDouble())
-//                                val marker = Marker()
-//                                markers.add(marker)
-//                                marker.position = tm.toLatLng()
-//                                marker.map = naverMap
-//                            }
-//                        }
-//
-//                        override fun onFailure(call: Call<ResultGetSearchLocation>, t: Throwable) {
-//                            Log.d("결과:", "실패 : $t")
-//                        }
-//                    })
-//            }
+//                    //Gson을 Kotlin에서 사용 가능한 object로 만든다.
+//                    val gson = GsonBuilder().create()
+//                    val searchResult = Gson().fromJson(body, ResultGetSearchLocation::class.java)
+                            val resultList = view.findViewById<RecyclerView>(R.id.resultList)
+                            activity?.runOnUiThread {
+                                resultList.adapter =
+                                    body?.let { it -> RecyclerViewAdapter(it) }
+                            }
+                            //
+
+                            val wherex = body?.items?.get(0)?.mapx
+                            val wherey = body?.items?.get(0)?.mapy
+
+                            // 첫번째 검색 결과 좌표로 지도 이동
+                            var tm = Tm128(wherex!!.toDouble(), wherey!!.toDouble())
+                            val cameraUpdate = CameraUpdate.scrollTo(tm.toLatLng())
+                                .animate(CameraAnimation.Fly, 1000)
+                            naverMap.moveCamera(cameraUpdate)
+
+                            for (item in body.items) {
+                                tm = Tm128(item.mapx!!.toDouble(), item.mapy!!.toDouble())
+                                val marker = Marker()
+                                markers.add(marker)
+                                marker.position = tm.toLatLng()
+                                marker.map = naverMap
+                            }
+                        }
+
+                        override fun onFailure(call: Call<ResultGetSearchLocation>, t: Throwable) {
+                            Log.d("결과:", "실패 : $t")
+                        }
+                    })
+            }
     }
 
     override fun onMapReady(naverMap: NaverMap) {
