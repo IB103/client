05458f6df94cac7b64943f54cbe866e46597f678
SeoMiran <tjalfks0815@hansung.ac.kr>
2023-02-07 11:36:59 +0900

    hi

--- app/src/main/java/com/hansung/capstone/MainActivity.kt
+++ app/src/main/java/com/hansung/capstone/MainActivity.kt
@@ -17,4 +17,5 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNavigationView?.setupWithNavController(navController)
     }
+    //hi
 }
