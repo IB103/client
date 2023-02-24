d1158879536f74c4e18c321ff826fcd979a4198d
yootal <khyoo96@naver.com>
2023-02-24 23:35:18 +0900

    간격 수정

--- app/src/main/java/com/hansung/capstone/board/BoardAdapterDecoration.kt
+++ app/src/main/java/com/hansung/capstone/board/BoardAdapterDecoration.kt
@@ -13,21 +13,24 @@ class BoardAdapterDecoration : RecyclerView.ItemDecoration() {
     ) {
         super.getItemOffsets(outRect, view, parent, state)
 
-        val position = parent.getChildAdapterPosition(view)
-        val count = state.itemCount
+//        val position = parent.getChildAdapterPosition(view)
+//        val count = state.itemCount
         val offset = 10
 
-        when (position) {
-            0 -> {
-                outRect.top = offset
-                outRect.bottom = offset
+        outRect.top = offset
+        outRect.bottom = offset
 
-            }
-            count - 1 -> outRect.bottom = offset
-            else -> {
-                outRect.top = offset
-                outRect.bottom = offset
-            }
-        }
+//        when (position) {
+//            0 -> {
+//                outRect.top = offset
+//                outRect.bottom = offset
+//
+//            }
+//            count - 1 -> outRect.bottom = offset
+//            else -> {
+//                outRect.top = offset
+//                outRect.bottom = offset
+//            }
+//        }
     }
 }
