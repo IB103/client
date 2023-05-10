c7f2c9f938190efad8098a2d62bd2d346ae89497
yootal <khyoo96@naver.com>
2023-05-10 18:56:31 +0900

    다시

--- app/src/main/java/com/hansung/capstone/retrofit/Permissions.kt
+++ app/src/main/java/com/hansung/capstone/retrofit/Permissions.kt
@@ -4,20 +4,33 @@ import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
 import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat.checkSelfPermission
 
 object Permissions {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
-    val permissionsLocation = arrayOf(
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-        Manifest.permission.POST_NOTIFICATIONS
-    )
-    val permissionsCamera = arrayOf(
-        Manifest.permission.CAMERA,
-        Manifest.permission.WRITE_EXTERNAL_STORAGE
-    )
+    val permissionsLocation = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.POST_NOTIFICATIONS
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+        )
+    }
+    val permissionsCamera = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.MANAGE_EXTERNAL_STORAGE,
+        )
+    }
+    else{
+        arrayOf(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
 }
 
 object PermissionUtils {
